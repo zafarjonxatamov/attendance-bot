3f4ac4778df31bd7eda2e08db32a3cd558b1727b
zafarjonxatamov <zxatamovjm@gmail.com>
2026-07-29 01:44:25 +0500

    Add ALLOWED_DISTANCE for metric conversion
    Added ALLOWED_DISTANCE constant for distance measurement.

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-﻿TELEGRAM_TOKEN = "8639025127:AAHxfuWmKNDMX81wQg1SAl-7E8XVaex5iIE"
+TELEGRAM_TOKEN = "8639025127:AAHxfuWmKNDMX81wQg1SAl-7E8XVaex5iIE"
 ADMIN_ID = 1819574194  # O'zingizning Telegram ID raqamingiz
 
 # Korxona koordinatalari (Qo'qon shahri misolida)
 OFFICE_LAT = 40.5333
 OFFICE_LON = 70.9333
 MAX_DISTANCE_KM = 2.0  # 2 km radius
+
+# Kod ishlashi uchun zarur bo'lgan metr o'lchami (2 km = 2000 metr)
+ALLOWED_DISTANCE = int(MAX_DISTANCE_KM * 1000)
