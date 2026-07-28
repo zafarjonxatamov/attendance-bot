78c6dbaf013f7c1f416d506d7af8f6c1488ea5ee
zafarjonxatamov <zxatamovjm@gmail.com>
2026-07-29 02:25:47 +0500

    Reformat config.py with comments for clarity

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TELEGRAM_TOKEN = "8639025127:AAHxfuWmKNDMX81wQg1SAl-7E8XVaex5iIE"
-ADMIN_ID = 1819574194
+ADMIN_ID = 1819574194  # Sizning Telegram ID raqamingiz
 
 # Korxona koordinatalari
 OFFICE_LAT = 40.5333
