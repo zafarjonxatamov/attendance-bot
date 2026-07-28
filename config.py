3839cea625c4c6f48e67640b03217058be190b0a
zafarjonxatamov <zxatamovjm@gmail.com>
2026-07-29 01:55:03 +0500

    Reorganize configuration variables in config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 TELEGRAM_TOKEN = "8639025127:AAHxfuWmKNDMX81wQg1SAl-7E8XVaex5iIE"
-ADMIN_ID = 1819574194  # O'zingizning Telegram ID raqamingiz
+ADMIN_ID = 1819574194
 
-# Korxona koordinatalari (Qo'qon shahri misolida)
+# Korxona koordinatalari
 OFFICE_LAT = 40.5333
 OFFICE_LON = 70.9333
-MAX_DISTANCE_KM = 2.0  # 2 km radius
-
-# Kod ishlashi uchun zarur bo'lgan metr o'lchami (2 km = 2000 metr)
+MAX_DISTANCE_KM = 2.0
 ALLOWED_DISTANCE = int(MAX_DISTANCE_KM * 1000)
